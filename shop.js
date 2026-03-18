7bf1f2a27efdc6a6a3b73fb0a8504d16696396d2
mervinsaib <mervinsaib@gmail.com>
2026-03-18 19:37:59 +0100

    nieuwe feature toegevoegd aantallen bundelen

--- shop.js
+++ shop.js
@@ -37,11 +37,20 @@ function renderBasket() {
     if (cartButtonsRow) cartButtonsRow.style.display = "none";
     return;
   }
+  
+  // Group products by name and count quantities
+  const productCounts = {};
   basket.forEach((product) => {
+    productCounts[product] = (productCounts[product] || 0) + 1;
+  });
+  
+  // Render grouped products with quantities
+  Object.keys(productCounts).forEach((product) => {
     const item = PRODUCTS[product];
     if (item) {
       const li = document.createElement("li");
-      li.innerHTML = `<span class='basket-emoji'>${item.emoji}</span> <span>${item.name}</span>`;
+      const quantity = productCounts[product];
+      li.innerHTML = `<span class='basket-emoji'>${item.emoji}</span> <span>${quantity}x ${item.name}</span>`;
       basketList.appendChild(li);
     }
   });
